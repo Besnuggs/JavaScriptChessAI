8f36df1dd6ab38bd53c64abae58018a310b146b5
Brady Snuggs <dronebug360@gmail.com>
2018-12-09 19:18:50 -0700

    separated AI logic from general chess logic and chessboard logic

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var board;
 var cfg;
 var game;
-var positionCount;
+
 
 //Game Details
 var statusEl = $('#status'),
@@ -81,137 +81,6 @@ var onDragStart = function(source, piece, position, orientation) {
   };
   
 
-
-// minimax functions
-var minimaxRoot =function(depth, game, isMaximisingPlayer) {
-
-    var newGameMoves = game.moves();
-    var bestMove = -9999;
-    var bestMoveFound;
-
-    for(var i = 0; i < newGameMoves.length; i++) {
-        var newGameMove = newGameMoves[i];
-        game.move(newGameMove);
-        var value = minimax(depth - 1, game, !isMaximisingPlayer);
-        game.undo();
-        if(value >= bestMove) {
-            bestMove = value;
-            bestMoveFound = newGameMove;
-        }
-    }
-    return bestMoveFound;
-};
-
-  var minimax = function (depth, game, isMaximisingPlayer) {
-    positionCount++;
-    if (depth === 0) {
-        return -evaluateBoard(game.board());
-    }
-
-    var newGameMoves = game.moves();
-    if (isMaximisingPlayer) {
-        var bestMove = -9999;
-        for (var i = 0; i < newGameMoves.length; i++) {
-            game.move(newGameMoves[i]);
-            bestMove = Math.max(bestMove, minimax(depth - 1, game, !isMaximisingPlayer));
-            game.undo();
-        }
-        return bestMove;
-    } else {
-        var bestMove = 9999;
-        for (var i = 0; i < newGameMoves.length; i++) {
-            game.move(newGameMoves[i]);
-            bestMove = Math.min(bestMove, minimax(depth - 1, game, !isMaximisingPlayer));
-            game.undo();
-        }
-        return bestMove;
-    }
-};
-
-// Evaluation function and Piece Values
-var evaluateBoard = function (board) {
-    var totalEvaluation = 0;
-    for (var i = 0; i < 8; i++) {
-        for (var j = 0; j < 8; j++) {
-            totalEvaluation = totalEvaluation + getPieceValue(board[i][j]);
-        }
-    }
-    return totalEvaluation;
-};
-
-var getPieceValue = function (piece) {
-    if (piece === null) {
-        return 0;
-    }
-    var getAbsoluteValue = function (piece) {
-        if (piece.type === 'p') {
-            return 10;
-        } else if (piece.type === 'r') {
-            return 50;
-        } else if (piece.type === 'n') {
-            return 30;
-        } else if (piece.type === 'b') {
-            return 30 ;
-        } else if (piece.type === 'q') {
-            return 90;
-        } else if (piece.type === 'k') {
-            return 900;
-        }
-        throw "Unknown piece type: " + piece.type;
-    };
-
-    var absoluteValue = getAbsoluteValue(piece, piece.color === 'w');
-    return piece.color === 'w' ? absoluteValue : -absoluteValue;
-};
-
-var calculateBestMove = function() {
-    //generate all the moves for a given position
-    var newGameMoves = game.moves();
-    if (newGameMoves.length === 0) return;
-
-    let bestMove = null;
-    let bestValue = -9999;
-
-    //this 'for loop' is going through array of legal moves.
-    //game.move will play each move
-    for(var i = 0; i < newGameMoves.length; i++){
-        let newGameMove = newGameMoves[i];
-        game.move(newGameMove)
-        //take the negative as AI plays as black
-        let boardValue = -evaluateBoard(game.board())
-        game.undo()
-        
-        if(boardValue > bestValue){
-            bestValue = boardValue;
-            bestMove = newGameMove
-        }
-    }
-    game.move(bestMove);
-    board.position(game.fen())
-    updateStatus();
-};
-  
-var makeBestMove = function () {
-    var bestMove = getBestMove(game);
-    game.move(bestMove);
-    board.position(game.fen());
-};
-
-
-var getBestMove = function (game) {
-    positionCount = 0;
-    var depth = parseInt($('#search-depth').find(':selected').text());
-    var d = new Date().getTime();
-    var bestMove = minimaxRoot(depth, game, true);
-    var d2 = new Date().getTime();
-    var moveTime = (d2 - d);
-    var positionsPerS = ( positionCount * 1000 / moveTime);
-
-    $('#position-count').text(positionCount);
-    $('#time').text(moveTime/1000 + 's');
-    $('#positions-per-s').text(positionsPerS);
-    return bestMove;
-};
 
   // update the board position after the piece snap
   // for castling, en passant, pawn promotion
